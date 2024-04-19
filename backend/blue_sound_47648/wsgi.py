e37adf1e0e1d91042daeeafe8d80067aa8b117b5
Crowdbotics <team@crowdbotics.com>
2024-04-19 19:55:15 +0000

    Initial commit

---
+++ backend/blue_sound_47648/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for blue_sound_47648 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "blue_sound_47648.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-sound-47648",
+  url: "https://blue-sound-47648.botics.co",
+  api: "https://blue-sound-47648.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
